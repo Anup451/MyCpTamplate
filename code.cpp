8049e2232fc50edd019a3e1c63d06c0437ae968b
Anup Jujagar <78673834+Anup451@users.noreply.github.com>
2023-11-27 15:01:32 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,4 +1,7 @@
-// Anup Suresh Jujagar
+// Auther :Anup Suresh Jujagar
+// B.tech :Indian Institute Of Information Technology, Dharwad
+// Branch :Computer Science And Engineering
+// Linkedin :https://www.linkedin.com/in/anup-jujagar-531430203/
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -33,50 +36,21 @@ typedef unordered_map<ll,ll> umll;
 typedef map<ll,ll> mll;
 
 // Macros
-#define ff first
-#define ss second
+#define f first
+#define s second
 #define pb push_back
 #define mp make_pair
-#define fl(i,n) for(int i=0;i<n;i++)
+#define f(i,n) for(int i=0;i<n;i++)
 #define rl(i,m,n) for(int i=n;i>=m;i--)
-#define py cout<<"YES\n";
-#define pm cout<<"-1\n";
-#define pn cout<<"NO\n";
 #define vr(v) v.begin(),v.end()
 #define rv(v) v.end(),v.begin()
 
-// Operator overloads
-template<typename T1, typename T2> // cin >> pair<T1, T2>
-istream& operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); }
-template<typename T> // cin >> vector<T>
-istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
-template<typename T1, typename T2> // cout << pair<T1, T2>
-ostream& operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
-template<typename T> // cout << vector<T>
-ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
-
-// Utility functions
-template <typename T>
-void print(T &&t)  { cout << t << "\n"; }
-void printarr(ll arr[], ll n){fl(i,n) cout << arr[i] << " ";cout << "\n";}
-template<typename T>
-void printvec(vector<T>v){ll n=v.size();fl(i,n)cout<<v[i]<<" ";cout<<"\n";}
-template<typename T>
-ll sumvec(vector<T>v){ll n=v.size();ll s=0;fl(i,n)s+=v[i];return s;}
 
 // Mathematical functions
 ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
 ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
 ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
 ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
-
-//Graph-dfs
-// bool gone[MN];
-// vector<int> adj[MN];
-// void dfs(int loc){
-//     gone[loc]=true;
-//     for(auto x:adj[loc])if(!gone[x])dfs(x);
-// }
 
 //Sorting
 bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
@@ -91,15 +65,14 @@ bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)re
 bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
 bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
 
-//Code by Anup Suresh Jujagar
-//Language C++
-//Practice->Success
-
 //Code
 void solve()
 {
     ll n;
     cin>>n;
+    
+    
+    
 }
 //Main
 int main()
