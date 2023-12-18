2746abed4f648b5477f4ec39cf9cfbdebc922914
Anup451 <anupjujagar121@gmail.com>
2023-12-18 17:06:26 +0530

    upadted

---
+++ Interactive Programs/Interactive.cpp
@@ -0,0 +1,80 @@
+// Auther :Anup Suresh Jujagar
+// B.tech :Indian Institute Of Information Technology, Dharwad
+// Branch :Computer Science And Engineering
+// Linkedin :https://www.linkedin.com/in/anup-jujagar-531430203/
+
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <sys/resource.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace chrono;
+// using namespace __gnu_pbds;
+
+//Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define SetBitCoder cout.tie(NULL);
+# define int long long
+//Aliases
+using ll= long long;
+using lld= long double;
+using ull= unsigned long long;
+
+//Constants
+const lld pi= 3.141592653589793238;
+const ll INF= LONG_LONG_MAX;
+const ll mod=1e9+7;
+
+// Mathematical functions
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
+ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
+
+//Sorting
+bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
+
+//Bits
+string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
+ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+
+//Check
+bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
+bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
+
+//Code
+void solve()
+{
+int m,n,i,j;
+cin>>n;
+vector<int>v(n);
+for(int i=0;i<n;i++)cin>>v[i];
+for(auto it:v)cout<<it<<" ";
+cout<<endl;
+
+    
+}
+//Main
+signed main()
+{
+    Code By SetBitCoder
+    ll t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        solve();
+    }
+    // solve();
+    // fl(i,t) //Kickstart
+    // {
+    //     cout<<"Case #"<<i+1<<": ";
+    //     solve();
+    //     cout<<"\n";
+    // }
+    return 0;
+}
+//End
